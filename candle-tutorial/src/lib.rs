4e425356352e4e09c0b7bb47ad90c96e6d648be4
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2025-11-30 21:01:09 +0700

    feat: create candle tutorial project

---
+++ candle-tutorial/src/lib.rs
@@ -0,0 +1,20 @@
+/// # Title
+///
+/// This function adds two numbers.
+/// ```
+/// assert_eq!(add(2, 3), 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
